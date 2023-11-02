4e3c3a9286730b59a9e1da6fe891e0c8615cb476
shaheem097 <shaheemsha097@gmail.com>
2023-11-02 15:37:17 +0530

    OTP signup,resend,timer

---
+++ src/Component/user/OtpVerification.jsx
@@ -0,0 +1,303 @@
+import React, { useRef, useState ,useEffect} from 'react'
+import { useNavigate } from "react-router-dom";
+import axios from "../../Axios/axios";
+import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { signInWithPhoneNumber } from "firebase/auth";
+import { auth} from "../../firebase/config.js";
+import { RecaptchaVerifier } from "firebase/auth";
+import { setUserDetails } from "../../Redux/Reducers/singleReducer";
+
+
+
+
+function OtpVerification() {
+
+    
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const [otpInputForm, setOtpInpuForm] = useState(false);
+    const [otp, setOtp] = useState(["", "", "", "", "", ""]);
+    const inputRefs = [useRef(), useRef(), useRef(), useRef(), useRef(), useRef()];
+    const [otpTimer, setOtpTimer] = useState(0);
+    const [otpSent, setOtpSent] = useState(false);
+    const [otpExpired, setOtpExpired] = useState(false);
+    const [showResendButton, setShowResendButton] = useState(false);
+    const [phone,setPhone]=useState("")
+
+    
+  const handleOtpChange = (index, value) => {
+
+    if (value === '') {
+      const newOtp = [...otp];
+      newOtp[index] = '';
+      setOtp(newOtp);
+      if (index > 0) {
+        inputRefs[index - 1].current.focus(); 
+      }
+    } else if (!isNaN(value)) {
+      const newOtp = [...otp];
+      newOtp[index] = value;
+      setOtp(newOtp);
+      if (index < 5) {
+        
+        inputRefs[index + 1].current.focus(); 
+      }
+    }
+  };
+
+  function onCaptchaVerify(){
+    if(!window.recaptchaVerifier ){
+      window.recaptchaVerifier = new RecaptchaVerifier(auth, 'recaptcha-container', {
+        'size': 'invisible',
+        callback: (response) => {
+      sentOtp()
+        },
+        'expired-callback': () => {
+        
+        }
+      },auth);
+    }
+  }
+  
+  const checkPhoneExistence = async (phone) => {
+  
+    try {
+      const response = await axios.post('/checkPhoneNumber', {
+        phone,
+      });
+      console.log(response.data,"dataphon");
+      if(response.data.status===true){
+      
+  return true
+      } 
+      else if(response.data.blocked===true){
+        return {blocked:true}
+      }
+      else if(response.data.status===false){
+       
+        return false
+      }
+      
+    } catch (error) {
+      console.error(error);
+      return false; 
+    }
+  };
+  const startOtpTimer = () => {
+    const otpExpiryTime = 60; // OTP validity period in seconds
+    setOtpTimer(otpExpiryTime);
+  
+    let timerId = setInterval(() => {
+      setOtpTimer((prevTimer) => prevTimer - 1);
+  
+      if (otpTimer <= 0) {
+        clearInterval(timerId);
+        handleOtpTimerExpiration();
+      }
+    }, 60000);
+  };
+
+  const handleOtpNumber =(e)=>{
+    setPhone(e.target.value)
+    }
+
+  const sentOtp = async() => {
+   onCaptchaVerify()
+
+
+   const phoneExists = await checkPhoneExistence(phone);
+
+   if (phoneExists&&!phoneExists.blocked) {
+    const appVerifier = window.recaptchaVerifier;
+    const ph = '+91' + phone;
+    console.log(ph);
+    signInWithPhoneNumber(auth, ph, appVerifier)
+      .then((confirmationResult) => {
+        window.confirmationResult = confirmationResult;
+        toast.success("OTP sent to your Number");
+        setOtpInpuForm(true)
+        setOtpSent(true); // OTP is sent
+        startOtpTimer()// Start the OTP timer
+        setShowResendButton(false);
+        setOtpExpired(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } 
+  else if(phoneExists.blocked){
+ 
+    toast.error("User blocked by Admin");
+  }
+  else {
+        toast.error("Phone number doesn't exist");
+  }
+  };
+
+  const handleOtpTimerExpiration = () => {
+    console.log("Timer expiredddddddddd");
+    setOtpSent(false);
+    setOtpTimer(0);
+    setOtpExpired(true); // Set otpExpired to true when the timer expires
+    setShowResendButton(true); // Show "Resend OTP" button
+  };
+  
+  useEffect(() => {
+    let timerId;
+
+    if (otpSent && otpTimer > 0) {
+      // Set up a timer to decrement the timer value every second
+      timerId = setInterval(() => {
+        setOtpTimer((prevTimer) => prevTimer - 1);
+
+        if (otpTimer <= 0) {
+          clearInterval(timerId);
+          handleOtpTimerExpiration();
+        }
+      }, 1000);
+    }
+
+    // Clean up the timer when the component unmounts or when the OTP is verified
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [otpSent, otpTimer]);
+
+
+  
+  const resendOtp = () => {
+    // Add your logic to resend OTP here
+    // You can call the sentOtp function again to resend OTP
+    sentOtp();
+    setOtpExpired(false); // Reset the OTP expiration flag
+    setShowResendButton(false);
+   
+  };
+ 
+  
+
+  function verifyOtp() {
+    const enteredOtp = otp.join('');
+    window.confirmationResult.confirm(enteredOtp)
+      .then(async (res) => {
+        axios.post("/otpLogin", { phone: phone })
+          .then((response) => {
+            console.log(response.data.status);
+            
+            if (response.data.status) {
+              localStorage.setItem("userAccessToken", response?.data?.response?.userData?.token);
+              dispatch(setUserDetails({ payload: response?.data?.response?.userData }));
+              toast.success("OTP Login successful!");
+              navigate("/");
+            } else {
+              toast.warn("Something Error");
+              
+            }
+          })
+          .catch((error) => {
+            if (error.code === "auth/invalid-verification-code") {
+              toast.error("Invalid OTP. Please try again.");
+            } else {
+              toast.error("An error occurred. Please try again later.");
+            }
+   
+          });
+      })
+      .catch((error) => {
+        if (error.code === "auth/invalid-verification-code") {
+          toast.error("Invalid OTP. Please try again.");
+        } else {
+          toast.error("An error occurred. Please try again later.");
+        }
+      
+      });
+  }
+  
+  
+
+
+  return (
+    <div>
+          
+        <div>
+            <label htmlFor="phoneNumber" className="text-gray-300">
+              Phone Number
+            </label>
+            <input
+              id="phoneNumber"
+              name="phoneNumber"
+              type="text"
+              value={phone}
+              onChange={handleOtpNumber}
+              required
+              className="w-full p-2 bg-gray-700 border border-gray-600 rounded-lg focus:outline-none focus:ring focus:border-blue-400 text-white"
+              placeholder="Phone Number"
+            />
+            <div style={{ height: "20px" }}></div>
+          </div>
+          <div>
+          <button
+          onClick={sentOtp}
+          type="button"
+          className="group relative w-full flex justify-center py-2 px-4 mb-7 border border-transparent text-sm font-medium rounded-md text-white bg-[#030712] hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:bg-[#030712]"
+          disabled={otpSent} // Disable the button if OTP has already been sent
+        >
+          Send OTP
+        </button>
+          </div>
+
+         <div id="recaptcha-container" className="mb-5"></div>
+
+
+         {otpInputForm && (
+  <div className="flex justify-between items-center">
+         
+    
+          {otp.map((value, index) => (
+    <input
+      key={index}
+      className="w-12 h-12 text-center border rounded-md shadow-sm focus:border-teal-500 focus:ring-teal-500"
+      type="text"
+      maxLength="1"
+      pattern="[0-9]"
+      inputMode="numeric"
+      autoComplete="one-time-code"
+      required
+      value={value}
+      onChange={(e) => handleOtpChange(index, e.target.value)}
+      ref={inputRefs[index]}
+    />
+  ))}<button
+  className={`bg-teal-500 text-white font-bold py-2 px-4 rounded focus-outline-none focus-shadow-outline ${
+    otp.some(value => value === '') ? 'disabled-button' : 'hover-enabled-button'
+  }`}
+  type="button"
+  onClick={verifyOtp}
+  disabled={otp.some(value => value === '')}
+>
+  Verify
+</button>
+
+  </div>
+
+
+          
+          
+)}
+    <div>{otpSent && !otpExpired && <p className="text-center text-white">OTP expires in {otpTimer} seconds</p>}</div>
+      {otpExpired && showResendButton && (
+        <p className="text-center text-red-500">
+          OTP has expired.{" "}
+          <button onClick={resendOtp} className="text-blue-500 underline">
+            Resend OTP
+          </button>
+        </p>
+      )}
+    </div>
+  )
+}
+
+export default OtpVerification
--- src/pages/user/Signup.jsx
+++ src/pages/user/Signup.jsx
@@ -1,9 +1,12 @@
-import React, { useState} from "react";
+import React, { useEffect, useRef, useState} from "react";
 import axios from "../../Axios/axios";
 import { useDispatch } from "react-redux";
 import { setUserDetails } from "../../Redux/Reducers/singleReducer";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import { signInWithPhoneNumber } from "firebase/auth";
+import { auth} from "../../firebase/config.js";
+import { RecaptchaVerifier } from "firebase/auth";
 import "react-toastify/dist/ReactToastify.css";
 
 
@@ -26,6 +29,14 @@ function Signup() {
     password: "",
   });
 
+  const [otpInputForm, setOtpInpuForm] = useState(false);
+  const [otp, setOtp] = useState(["", "", "", "", "", ""]);
+  const inputRefs = [useRef(), useRef(), useRef(), useRef(), useRef(), useRef()];
+  const [otpTimer, setOtpTimer] = useState(0);
+  const [otpSent, setOtpSent] = useState(false);
+  const [otpExpired, setOtpExpired] = useState(false);
+  const [showResendButton, setShowResendButton] = useState(false);
+
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -39,6 +50,130 @@ function Signup() {
       ...formErrors,
       [name]: "",
     });
+  };
+ 
+  const handleOtpChange = (index, value) => {
+
+    if (value === '') {
+      const newOtp = [...otp];
+      newOtp[index] = '';
+      setOtp(newOtp);
+      if (index > 0) {
+        inputRefs[index - 1].current.focus(); 
+      }
+    } else if (!isNaN(value)) {
+      const newOtp = [...otp];
+      newOtp[index] = value;
+      setOtp(newOtp);
+      if (index < 5) {
+        
+        inputRefs[index + 1].current.focus(); 
+      }
+    }
+  };
+  function onCaptchaVerify(){
+   console.log("cpachayil vannu");
+   if(!window.recaptchaVerifier ){
+      console.log("window capchaillaaaa");
+    
+      window.recaptchaVerifier = new RecaptchaVerifier(auth, 'recaptcha-container', {
+        'size': 'invisible',
+        callback: (response) => {
+      sentOtp()
+        },
+        'expired-callback': () => {
+        
+        }
+      },auth);
+    }
+  }
+
+  const sentOtp = async() => {
+    console.log("otp functionil vannu");
+    onCaptchaVerify()
+
+     const appVerifier = window.recaptchaVerifier;
+     console.log(appVerifier,"appverifierr");
+     const ph = '+91' + formData.phone;
+     console.log(ph);
+     signInWithPhoneNumber(auth, ph, appVerifier)
+       .then((confirmationResult) => {
+         window.confirmationResult = confirmationResult;
+         toast.success("OTP sent to your Number");
+         setOtpInpuForm(true)
+         setOtpSent(true); // OTP is sent
+         startOtpTimer()// Start the OTP timer
+         setShowResendButton(false);
+         setOtpExpired(false);
+       })
+       .catch((error) => {
+         console.log(error);
+       });
+   
+  
+   };
+
+   useEffect(() => {
+    let timerId;
+
+    if (otpSent && otpTimer > 0) {
+      timerId = setInterval(() => {
+        setOtpTimer((prevTimer) => prevTimer - 1);
+
+        if (otpTimer <= 0) {
+          clearInterval(timerId);
+          handleOtpTimerExpiration();
+        }
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [otpSent, otpTimer]);
+
+   const startOtpTimer = () => {
+    const otpExpiryTime = 60; // OTP validity period in seconds
+    setOtpTimer(otpExpiryTime);
+  
+    let timerId = setInterval(() => {
+      setOtpTimer((prevTimer) => prevTimer - 1);
+  
+      if (otpTimer <= 0) {
+        clearInterval(timerId);
+        handleOtpTimerExpiration();
+      }
+    }, 60000);
+  };
+
+  const handleOtpTimerExpiration = () => {
+    console.log("Timer expiredddddddddd");
+    setOtpSent(false);
+    setOtpTimer(0);
+    setOtpExpired(true); // Set otpExpired to true when the timer expires
+    setShowResendButton(true); // Show "Resend OTP" button
+  };
+
+  function verifyOtp() {
+    const enteredOtp = otp.join('');
+    window.confirmationResult.confirm(enteredOtp)
+      .then(async (res) => {
+        registerUser()
+      })
+      .catch((error) => {
+        if (error.code === "auth/invalid-verification-code") {
+          toast.error("Invalid OTP. Please try again.");
+        } else {
+          toast.error("An error occurred. Please try again later.");
+        }
+      
+      });
+  }
+
+  const resendOtp = () => {
+    sentOtp();
+    setOtpExpired(false); // Reset the OTP expiration flag
+    setShowResendButton(false);
   };
  
   const handleSubmit = async (e) => {
@@ -82,53 +217,35 @@ function Signup() {
     
     if (Object.keys(errors).length === 0) {
 
-      console.log(formData.email,"hggfhhhfhh");
-      // Send email verification link
-     
-      // const email = formData.email
-    
-      // const actionCodeSettings = {
-      //   url: "http://localhost:3000", // Replace with your actual URL
-      //   handleCodeInApp: true,
-      // };
-   
-      //   const response = await sendSignInLinkToEmail(auth, email, actionCodeSettings);
-   
-            axios.post("/signup", formData).then((response) => {
-              console.log("vannnnnnnnnnnnn");
-              if (response?.data?.token?.status === true) {
-                localStorage.setItem("userAccessToken", response?.data?.token?.userData?.token);
-              
-                dispatch(setUserDetails({ payload: response?.data?.token?.userData}));
-                toast.success("Registration successful!");
-                navigate("/");
-              } else {
-                if (response.data.token.message === "Username,Email & Phonenumber Allready Exist") {
-                  toast.warn("Username,Email & Phonenumber Allready Exist!");
-                } else if (response.data.token.message === "Email & Phonenumber Allready Exist") {
-                  console.log(response);
-                  toast.warn("Email & Phonenumber Allready Exist !");
-                }  else if (response.data.token.message === "Username & Email Allready Exist") {
-                  console.log(response);
-                  toast.warn("Username & Email Allready Exist!");
-                }else if (response.data.token.message === "Username & Phone Allready Exist") {
-                  console.log(response);
-                  toast.warn("Username & Phone Allready Exist!");
-                }else if (response.data.token.message === "Username Allready Exist") {
-                  console.log(response);
-                  toast.warn("Username Allready Exist!");
-                }else if (response.data.token.message === "Email Allready Exist") {
-                  console.log(response);
-                  toast.warn("Email Allready Exist!");
-                }
-                else {
-                  toast.warn("Phone number already exists!");
-                }
-              }
-            });
+      await axios.post('/checkExistingData', formData).then((response)=>{
+        if (response?.data?.status === true) {
+         sentOtp()
+
+        } else {
+              toast.error(response.data.message);
+            
+            }
+      })
+
           }
         };
 
+        const registerUser=()=>{
+
+       
+        axios.post("/signup", formData).then((response) => {
+             
+          if (response?.data?.token?.status === true) {
+            localStorage.setItem("userAccessToken", response?.data?.token?.userData?.token);
+          
+            dispatch(setUserDetails({ payload: response?.data?.token?.userData}));
+            toast.success("Registration successful!");
+            navigate("/");
+          } else{
+            toast.error(response.data.token.message);
+          }
+        });
+      }
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-[#030712]">
@@ -136,6 +253,7 @@ function Signup() {
       <div className="flex items-center justify-center mb-4">
         <h2 className="text-2xl font-semibold">Signup</h2>
       </div>
+      {!otpInputForm && (
       <form onSubmit={handleSubmit}>
         <div className="space-y-4">
         <div>
@@ -154,11 +272,7 @@ function Signup() {
             {formErrors.username && (
               <p className="text-red-500">{formErrors.username}</p>
             )}
-            {formData.username.length < 15 && (
-              <p className="text-gray-300 text-right">
-                 {15 - formData.username.length}
-              </p>
-            )}
+          
           </div>
           <div>
             <label htmlFor="email" className="text-gray-300">
@@ -230,10 +344,55 @@ function Signup() {
             </a>
           </p>
         </div>
+        
       </form>
+ )}
+      <div id="recaptcha-container" className="mb-5"></div>
+
+      {otpInputForm && (
+       
+        <div className="flex justify-between items-center">
+
+                {otp.map((value, index) => (
+          <input
+            key={index}
+            className="w-12 h-12 text-black text-center border rounded-md shadow-sm focus:border-teal-500 focus:ring-teal-500"
+            type="text"
+            maxLength="1"
+            pattern="[0-9]"
+            inputMode="numeric"
+            autoComplete="one-time-code"
+            required
+            value={value}
+            onChange={(e) => handleOtpChange(index, e.target.value)}
+            ref={inputRefs[index]}
+          />
+        ))}<button
+        className={`bg-teal-500 text-white font-bold py-2 px-4 rounded focus-outline-none focus-shadow-outline ${
+          otp.some(value => value === '') ? 'disabled-button' : 'hover-enabled-button'
+        }`}
+        type="button"
+        onClick={verifyOtp}
+        disabled={otp.some(value => value === '')}
+      >
+        Verify
+      </button>    
+        </div>
+
+                
+      )}
+          <div>{otpSent && !otpExpired && <p className="text-center text-white">OTP expires in {otpTimer} seconds</p>}</div>
+      {otpExpired && showResendButton && (
+        <p className="text-center text-red-500">
+          OTP has expired.{" "}
+          <button onClick={resendOtp} className="text-blue-500 underline">
+            Resend OTP
+          </button>
+        </p>
+      )}
     </div>
   </div>
-  
+ 
   );
 }
 
